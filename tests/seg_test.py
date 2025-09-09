b90dc76ecf72286514c95060239b4e3f63fa3413
annazchen <azchen@uwaterloo.ca>
2025-09-09 15:59:41 -0400

    forgot to save recent changes on seg

--- tests/seg_test.py
+++ tests/seg_test.py
@@ -1,9 +1,11 @@
 import cv2
 import depthai as dai
 import blobconverter
+import time
+import numpy as np
 
 #grabbing yolov8 segmentation model
-seg_blob = blobconverter.from_zoo(name = "yolov8s-seg", shaves = 10)
+seg_blob = blobconverter.from_zoo(name = "yolov8s-seg", shaves = 10, data_type = "FP16")
 
 #intializing pipeline
 pipeline = dai.Pipeline()
@@ -30,3 +32,21 @@ seg_nn.out.link(xout_seg.input)
 xout_rgb = pipeline.createXLinkOut()
 xout_rgb.setStreamName("rgb")
 cam.preview.link(xout_rgb.input)
+
+with dai.Device(pipeline) as device:
+    q_rgb = device.getOutputQueue("rgb", maxSize = 4, blocking = False)
+    q_seg = device.getOutputQueue("segmentation", maxSize = 4, blocking = False)
+
+    while True:
+        in_rgb = q_rgb.tryGet()
+        in_seg = q_seg.tryGet()
+
+        if in_rgb is not None:
+            raw_frame = in_rgb.getRaw()
+            frame = np.array
+
+        0, n = time.monotonic(), 0
+        print("[INFO] running NN pipeline. Press q to quit")
+
+        #convert seg output to mask
+        masks = in_seg.getRaw()
