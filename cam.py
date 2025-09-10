c05b165752ce2643f73686d77c062802d8330fc7
annazchen <azchen@uwaterloo.ca>
2025-09-10 09:55:34 -0400

    created cam.py

---
+++ cam.py
@@ -0,0 +1,47 @@
+import cv2
+import depthai as dai
+import blobconverter
+import time
+import os 
+
+#initializing pipeline
+pipeline = dai.Pipeline()
+
+#cam specs
+cam = pipeline.createColorCamera()
+cam.setResolution(dai.ColorCameraProperties.SensorResolution.THE_4_K)
+cam.setColorOrder(dai.ColorCameraProperties.ColorOrder.BGR)
+cam.setInterleaved(False)
+cam.setFps(30)
+
+#output
+xout_rgb = pipeline.createXLinkOut()
+xout_rgb.setStreamName('rgb')
+cam.preview.link(xout_rgb.input)
+
+with dai.Device(pipeline) as device:
+    q_rgb = device.getOutputQueue('rgb', maxSize = 4, blocking = False)
+
+    t0, n = time.monotonic(), 0
+    print("[INFO] running NN pipeline. Press q to quit")
+
+    while True:
+        in_rgb = q_rgb.tryGet()
+
+        if in_rgb is not None:
+            rgb = in_rgb.getCvFrame()
+            bw = cv2.cvtColor(rgb, cv2.COLOR_BGR2GRAY)
+            cv2.imshow('oak-1 max, b/w', bw)
+        
+        cap = cv2.waitKey(1) & 0xFF
+        if cap == ord('c'):
+            filename = os.path.join("/home/user/Documents/loto-project/dataset",f"open_{int(time.time())}.jpg")
+            cv2.imwrite(filename, bw)
+            print(f"saved {filename}")
+
+        
+        if cv2.waitKey(1) == ord('q'):
+            break
+
+cv2.destroyAllWindows()
+
